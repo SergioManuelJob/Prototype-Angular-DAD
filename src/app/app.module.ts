f6a0404b19b8f30e1f13ab999a5efd1d24ac0f40
SergioManuelJob <suarezsergiomanuel69@gmail.com>
2022-10-04 20:03:53 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    ContactComponent,
+    ProfileComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
